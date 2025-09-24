ea189f941dfc064adad853c45b154197d979c6b9
SuleymanDD <sdemirel.2375@gmail.com>
2025-09-24 18:27:01 +0300

    Changes for Project
    - Some new UI changes.

--- Project/client/src/index.js
+++ Project/client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import "antd/dist/antd.css";
 import './index.css';
-import App from './App';
+import App from './components/App';
 import { ApolloProvider } from "@apollo/client/react";
 import client from "./apollo"
 
