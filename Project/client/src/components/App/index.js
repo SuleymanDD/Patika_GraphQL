3064459c38d55e29de272461439e214191d40656
SuleymanDD <sdemirel.2375@gmail.com>
2025-09-24 20:38:54 +0300

    Changes for Project
    - Added baseUrl thing.
    - Router process is ejected to project.
    - Added Header Menu design.
    - Added Home Page
    - Added New Post Page

--- Project/client/src/components/App/index.js
+++ Project/client/src/components/App/index.js
@@ -1,64 +1,26 @@
 import { Row, Col } from "antd";
+import { Routes, Route} from "react-router-dom"
 
 import styles from "./styles.module.css"
-import { Avatar, List, Skeleton } from 'antd';
 
-const data = [
-  {
-    "createdAt": "2025-04-26T19:13:38.338Z",
-    "name": "Let go boizz",
-    "avatar": "https://cdn.jsdelivr.net/gh/faker-js/assets-person-portrait/male/512/12.jpg",
-    "gender": "female",
-    "email": "youngboizzz@gmail.com",
-    "city": "Lake Lorenzo",
-    "birthdate": "1955-04-13T21:25:22.777Z",
-    "id": "7",
-    "address": "Lake Lorenzo"
-  },
-  {
-    "createdAt": "2025-04-27T03:03:49.295Z",
-    "name": "Israel Friesen",
-    "avatar": "https://avatars.githubusercontent.com/u/63156927",
-    "gender": "male",
-    "email": "Lizeth_Hagenes@hotmail.com",
-    "city": "Franeckitown",
-    "birthdate": "1964-07-01T13:57:11.140Z",
-    "id": "8"
-  },
-  {
-    "createdAt": "2025-04-27T12:14:11.838Z",
-    "name": "Terence Hilpert II",
-    "avatar": "https://avatars.githubusercontent.com/u/25132279",
-    "gender": "female",
-    "email": "Alva_Tillman@gmail.com",
-    "city": "North Aimee",
-    "birthdate": "1979-01-14T21:17:21.365Z",
-    "id": "9"
-  },
-]
+import HeaderMenu from "./HeaderMenu";
+
+// Pages
+import Home from "pages/Home"
+import NewPost from "pages/NewPost";
+
 function App() {
   return (
     <div className={styles.container}>
       <Row justify={"center"}>
-        <Col span={14} className={styles.content}>
-          <List
-            className="demo-loadmore-list"
-            loading={false}
-            itemLayout="horizontal"
-            //loadMore={loadMore}
-            dataSource={data}
-            renderItem={item => (
-              <List.Item>
-                <Skeleton avatar title={false} loading={item.loading} active>
-                  <List.Item.Meta
-                    avatar={<Avatar src={item.avatar} />}
-                    title={<a href="https://ant.design">{item.name}</a>}
-                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                  />
-                </Skeleton>
-              </List.Item>
-            )}
-          />
+        <Col span={14}>
+          <HeaderMenu />
+          <div className={styles.content}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<NewPost />} />
+            </Routes>
+          </div>
         </Col>
       </Row>
     </div>
--- Project/client/src/index.js
+++ Project/client/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import "antd/dist/antd.css";
 import './index.css';
 import App from './components/App';
 import { ApolloProvider } from "@apollo/client/react";
 import client from "./apollo"
+import {BrowserRouter as Router} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ApolloProvider client={client}>
+    <Router>
     <App />
+    </Router>
   </ApolloProvider>
 );
