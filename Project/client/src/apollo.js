aa829d92f2d572c95627b2395abb8222ea75a87d
SuleymanDD <sdemirel.2375@gmail.com>
2025-11-07 17:45:20 +0300

    Changes for Project
    - Real-time listing of posts

--- Project/client/src/apollo.js
+++ Project/client/src/apollo.js
@@ -1,7 +1,28 @@
-import { ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
+import { ApolloClient, InMemoryCache} from "@apollo/client";
+import { OperationTypeNode } from "graphql";
+import { split, HttpLink } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/",
+});
+
+const wsLink = new WebSocketLink(
+  new SubscriptionClient("ws://localhost:4000/", {
+  })
+);
+
+const splitLink = split(
+  ({ operationType }) => {
+    return operationType === OperationTypeNode.SUBSCRIPTION;
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:4000/" }),
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
