c21532844d5cdd68afb4049749aecabb227b7529
SuleymanDD <sdemirel.2375@gmail.com>
2025-11-27 17:36:10 +0300

    Changes for Project
    - Users are listing now on select box.

--- Project/client/src/pages/NewPost/NewPostForm.js
+++ Project/client/src/pages/NewPost/NewPostForm.js
@@ -1,58 +1,62 @@
 import React from 'react';
-import { Form, Input, Button, Select} from "antd";
+import { Form, Input, Button, Select } from "antd";
+import { useQuery } from '@apollo/client/react';
+import { GET_USERS } from './queries';
+import styles from './styles.module.css';
 
 const { Option } = Select;
 
 function NewPostForm() {
-  return (
-    <Form
-        name='basic'
-        initialValues={{ remember: true}}
-        autoComplete='off'
-    >
-        <Form.Item
-            name="title"
-            rules={[{required: true, message: "Please type your title!"}]}
-        >
-            <Input size='medium' placeholder='Title' />
-        </Form.Item>
 
-        <Form.Item
-            name="shortDescription"
-        >
-            <Input size='medium' placeholder='Short description' />
-        </Form.Item>
-        
-        <Form.Item
-            name="description"
-        >
-            <Input.TextArea size='large' placeholder='Description' />
-        </Form.Item>
+    const { loading: get_users_loading, data: users_data } = useQuery(GET_USERS);
 
-        <Form.Item
-            name="cover"
+    return (
+        <Form
+            name='basic'
+            initialValues={{ remember: true }}
+            autoComplete='off'
         >
-            <Input size='medium' placeholder='Cover' />
-        </Form.Item>
+            <Form.Item
+                name="title"
+                rules={[{ required: true, message: "Please type your title!" }]}
+            >
+                <Input size='medium' placeholder='Title' />
+            </Form.Item>
 
-        <Form.Item
-            name="user"
-            rules={[{required: true, message: "Please select a user!"}]}
-        >
-            <Select placeholder="Select A User">
-                <Option value="male">Male</Option>
-                <Option value="male">Male</Option>
-                <Option value="male">Male</Option>
-            </Select>
-        </Form.Item>
+            <Form.Item
+                name="shortDescription"
+            >
+                <Input size='medium' placeholder='Short description' />
+            </Form.Item>
 
-        <Form.Item>
-            <Button type='primary' htmlType='submit'>
-                Submit
-            </Button>
-        </Form.Item>
-    </Form>
-  )
+            <Form.Item
+                name="description"
+            >
+                <Input.TextArea size='large' placeholder='Description' />
+            </Form.Item>
+
+            <Form.Item
+                name="cover"
+            >
+                <Input size='medium' placeholder='Cover' />
+            </Form.Item>
+
+            <Form.Item
+                name="user"
+                rules={[{ required: true, message: "Please select a user!" }]}
+            >
+                <Select disabled={get_users_loading} loading={get_users_loading} placeholder="Select A User">
+                    {users_data && users_data.users.map((item) => <Option key={item.id} value={item.id}>{item.fullName}</Option>)}
+                </Select>
+            </Form.Item>
+
+            <Form.Item className={styles.buttons}>
+                <Button type='primary' htmlType='submit'>
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    )
 }
 
 export default NewPostForm
