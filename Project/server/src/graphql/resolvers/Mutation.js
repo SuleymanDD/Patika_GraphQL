90d8f6554d913e81eefcc600915d48eeaedf1550
SuleymanDD <sdemirel.2375@gmail.com>
2026-01-09 23:33:17 +0300

    Changes for Project
    - All user's mutation components are updated.

--- Project/server/src/graphql/resolvers/Mutation.js
+++ Project/server/src/graphql/resolvers/Mutation.js
@@ -49,47 +49,46 @@ export const Mutation = {
     },
 
     // User
-    createUser: (_, { data }, { pubsub, db }) => {
-        const user = {
-            id: nanoid(),
+    createUser: async(_, { data }, { pubsub, _db }) => {
+        const newUser = new _db.User({
             ...data,
-        }
+        })
 
-        db.users.push(user);
+        const user = await newUser.save();
         pubsub.publish("userCreated", { userCreated: user });
+
         return user;
     },
-    updateUser: (_, { id, data }, { pubsub, db }) => {
-        const user_index = db.users.findIndex((user) => user.id === id);
+    updateUser: async(_, { id, data }, { pubsub, _db }) => {
+        const is_user_exists = await _db.User.findById(id)
 
-        if (user_index === -1) {
+        if (!is_user_exists) {
             throw new Error("User is not found!");
         }
 
-        const updatedUser = {
-            ...db.users[user_index],
-            ...data,
-        }
+        const updatedUser = await _db.User.findByIdAndUpdate(id,data,{
+            new: true,
+        })
+        
         pubsub.publish("userUpdated", { userUpdated: updatedUser });
         return updatedUser;
     },
-    deleteUser: (_, { id }, { pubsub, db }) => {
-        const user_index = db.users.findIndex(user => user.id === id);
+    deleteUser: async(_, { id }, { pubsub, _db }) => {
+        const is_user_exists = await _db.User.findById(id)
 
-        if (user_index === -1) {
+        if (!is_user_exists) {
             throw new Error("User is not found!");
         }
 
-        const deletedUser = db.users[user_index];
-        db.users.splice(user_index, 1);
+        const deletedUser = await _db.User.findByIdAndDelete(id);
+
         pubsub.publish("userDeleted", { userDeleted: deletedUser });
         return deletedUser;
     },
-    deleteAllUsers: (_, __, { db }) => {
-        const length = db.users.length;
-        db.users.splice(0, length);
+    deleteAllUsers: async(_, __, { _db }) => {
+        const deletedUsers = await _db.User.deleteMany({}); 
 
-        return { count: length }
+        return { count: deletedUsers.deletedCount }
     },
 
     // Comment
