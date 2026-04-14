cc8683400787f12cf37aea2f73951f466f961cab
SuleymanDD <sdemirel.2375@gmail.com>
2026-04-14 20:46:09 +0300

    Initial commit for hasura-voting-app

---
+++ hasura-voting-app/client/src/App.js
@@ -0,0 +1,24 @@
+import {Routes, Route, Link} from "react-router-dom"
+
+import Questions from "./pages/Questions";
+import NewQuestion from "./pages/New";
+
+function App() {
+  return (
+    <div className="App">
+
+      <nav>
+        <Link to="/">Questions</Link>
+        <Link to="/new">New Questions</Link>
+      </nav>
+
+
+      <Routes>
+        <Route path="/" element={<Questions />}></Route>
+        <Route path="/new" element={<NewQuestion />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
